3368656ef58c6f21b4033f940c41c9cd48983000
zhtoo <820159571@qq.com>
2018-09-29 21:11:19 +0800

    add

--- moduleview/src/main/java/com/zht/moduleview/ViewFragment.java
+++ moduleview/src/main/java/com/zht/moduleview/ViewFragment.java
@@ -1,5 +1,8 @@
 package com.zht.moduleview;
 
+import android.os.Bundle;
+import android.view.View;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.zht.common.base.BaseFragment;
 
@@ -12,5 +15,11 @@ public class ViewFragment extends BaseFragment {
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_view;
+    }
+
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+        super.initView(view, savedInstanceState);
+
     }
 }
