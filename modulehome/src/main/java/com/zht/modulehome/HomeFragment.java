aafa82715731bb28ebf52e5bc6fba15832bdb3e0
zhtoo <820159571@qq.com>
2018-10-18 21:11:54 +0800

    add

--- modulehome/src/main/java/com/zht/modulehome/HomeFragment.java
+++ modulehome/src/main/java/com/zht/modulehome/HomeFragment.java
@@ -1,14 +1,24 @@
 package com.zht.modulehome;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.zht.common.OpenFileBySystem;
 import com.zht.common.base.BaseFragment;
+
+import java.net.URISyntaxException;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by ZhangHaitao on 2018/9/3.
@@ -31,8 +41,15 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
         view.findViewById(R.id.tv_module_two).setOnClickListener(this);
         view.findViewById(R.id.tv_module_fragment).setOnClickListener(this);
         view.findViewById(R.id.bt_home_file_path).setOnClickListener(this);
+        view.findViewById(R.id.bt_home_get_file_path).setOnClickListener(this);
 
         editText = view.findViewById(R.id.ed_home_file_path);
+
+        // Environment.getExternalStorageDirectory() + "/zerobook"
+
+        editText.setText(Environment.getExternalStorageDirectory()
+                + "/zerobook" +
+                "/courseFile/");
     }
 
     @Override
@@ -50,10 +67,75 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
             // 3. 应用内fragment的获取
             Intent intent = new Intent(getContext(), ShowFragmentActivity.class);
             startActivity(intent);
-        }else if(view.getId() == R.id.bt_home_file_path){
+        } else if (view.getId() == R.id.bt_home_file_path) {
             String s = editText.getText().toString();
-            OpenFileBySystem.openFileByPath(getContext(),s);
+            OpenFileBySystem.openFileByPath(getContext(), s);
+        } else if (view.getId() == R.id.bt_home_get_file_path) {
+            showFileChooser();
         }
+    }
+
+    public static final int FILE_SELECT_CODE = 100;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    Log.d(TAG, "File Uri: " + uri.toString());
+                    // Get the path
+                    String path = null;
+                    try {
+                        path = getPath(getContext(), uri);
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                    editText.setText(path);
+
+                    Log.d(TAG, "File Path: " + path);
+                    // Get the file instance
+                    // File file = new File(path);
+                    // Initiate the upload
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(getContext(), "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private static final String TAG = "ChooseFile";
+
+
+    public static String getPath(Context context, Uri uri) throws URISyntaxException {
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {"_data"};
+            Cursor cursor = null;
+            try {
+                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow("_data");
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                // Eat it  Or Log it.
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
     }
 
 
