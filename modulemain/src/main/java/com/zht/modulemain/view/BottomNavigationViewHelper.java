96aa615fda4b266deb3f87c1500cf5712246fdaf
zhanghaitao <zhanghaitao@huachenjie.com>
2022-12-15 15:48:17 +0800

    删除BottomNavigationView切换tab动画

--- modulemain/src/main/java/com/zht/modulemain/view/BottomNavigationViewHelper.java
+++ modulemain/src/main/java/com/zht/modulemain/view/BottomNavigationViewHelper.java
@@ -1,13 +1,15 @@
 package com.zht.modulemain.view;
 
 
+import android.annotation.SuppressLint;
 import android.util.Log;
+import android.util.TypedValue;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.lang.reflect.Field;
+import com.zht.modulemain.R;
 
 
 /**
@@ -16,24 +18,26 @@ import java.lang.reflect.Field;
 public class BottomNavigationViewHelper {
 
     /**
-     * 禁用切换模式
+     * 在 app:labelVisibilityMode="labeled" 时，删除BottomNavigationView切换tab动画
      *
      * @param view BottomNavigationView
      */
+    @SuppressLint("RestrictedApi")
     public static void disableShiftMode(BottomNavigationView view) {
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
         try {
-
+            int itemTextAppearanceActive = menuView.getItemTextAppearanceActive();
             for (int i = 0; i < menuView.getChildCount(); i++) {
                 BottomNavigationItemView child = (BottomNavigationItemView) menuView.getChildAt(i);
-                Class<?> clazz = child.getClass();
-//                Method setShifting = clazz.getDeclaredMethod("setShifting", boolean.class);
-//                setShifting.setAccessible(true);//设置允许访问
-//                setShifting.invoke(clazz,false);
-
-                Field isShifting = clazz.getDeclaredField("isShifting");
-                isShifting.setAccessible(true);
-                isShifting.setBoolean(clazz,false);
+                TextView smallLabel = child.findViewById(R.id.smallLabel);
+                TextView largeLabel = child.findViewById(R.id.largeLabel);
+                if (smallLabel != null && largeLabel != null) {
+                    float min = Math.min(smallLabel.getTextSize(), largeLabel.getTextSize());
+                    smallLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX, min);
+                    largeLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX, min);
+                    child.setTextAppearanceActive(itemTextAppearanceActive);
+                    child.setChecked(i == menuView.getSelectedItemId());
+                }
             }
         } catch (Exception e) {
             Log.e("BNVHelper", "Unable to get shift mode field", e);
